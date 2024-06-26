6fc2336887a3b5433ade065cd8f15565f7fc5e78
Uğur Alp <38879240+UAt23@users.noreply.github.com>
2024-06-26 13:39:39 +0300

    Updated the left part for screenshots

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Container, Grid, Typography, Paper, TextField, Box } from '@mui/material';
+import { Container, Grid, Typography, Paper, Box } from '@mui/material';
 import './App.css';
 
 const App = () => {
@@ -10,17 +10,21 @@ const App = () => {
       </Typography>
       <Grid container spacing={3}>
         <Grid item xs={6}>
-          <Paper elevation={3} style={{ padding: '16px', height: '80vh' }}>
-            <Typography variant="h6" gutterBottom>
+          <Paper elevation={3} style={{ display: 'flex', flexDirection: 'column', padding: '16px', height: '80vh' }}>
+            <Typography style={{ flex: 1 }} variant="h6" gutterBottom>
               Paste Text Here
             </Typography>
-            <TextField
-              fullWidth
-              multiline
-              rows={24}
-              variant="outlined"
-              placeholder="Paste your reading passage here..."
-            />
+            <Box
+              sx={{
+                flex: 98,
+                border: '1px dashed grey',
+                padding: '16px',
+                textAlign: 'center',
+                overflowY: 'auto'
+              }}
+              contentEditable
+            >
+            </Box>
           </Paper>
         </Grid>
         <Grid item xs={6}>
